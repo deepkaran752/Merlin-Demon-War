28a5c60c36eadd6f70a9f34ad23015c92a86e85d
Deepkaran Singh <63723207+deepkaran752@users.noreply.github.com>
2023-05-19 22:38:55 +0530

    Add files via upload

---
+++ Scripts/CardData.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Card",menuName ="CardGame/Card")]
+public class CardData : ScriptableObject
+{
+    public enum DamageType
+    {
+        Fire,
+        Ice,
+        Both,
+        Destruct
+    }
+    public string cardTitle;
+    public string description;
+    public int cost;
+    public int damage;
+    public DamageType damageType;
+    public Sprite cardImage;
+    public Sprite frameImage;
+    public int NumberInDeck;
+    public bool isDefenceCard = false;
+    public bool isMirrorCard = false;
+    public bool isMulti = false;
+    public bool isDestruct = false;
+}
